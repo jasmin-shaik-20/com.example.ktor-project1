90e396760623f7ceb3c30164e5dae41865ff8f8d
jasmin.sk <jasmin.s@marketsimplified.com>
2023-08-09 17:48:09 +0530

    added

--- src/main/kotlin/com/example/plugins/Database.kt
+++ src/main/kotlin/com/example/plugins/Database.kt
@@ -1,20 +1,23 @@
 package com.example.plugins
 
 import com.example.dao.*
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
-fun configureDatabase() {
-    Database.connect(
-        url = "jdbc:postgresql://localhost:5432/data",
-        driver = "org.postgresql.Driver",
-        user = "postgres",
-        password = "Jasmin@20"
-    )
+fun Application.configureDatabase() {
+    val config = HoconApplicationConfig(ConfigFactory.load())
+    val driver = config.property("ktor.database.driver").getString()
+    val url = config.property("ktor.database.url").getString()
+    val user = config.property("ktor.database.user").getString()
+    val password = config.property("ktor.database.password").getString()
+
+    Database.connect(url = url, driver = driver, user = user, password = password)
 
     transaction {
         SchemaUtils.createMissingTablesAndColumns(Users, Profile)
